caad0410c09bd4f31aa79e12165662684ea5fb02
theorNg <thaonp411@gmail.com>
2025-04-11 21:47:31 +0700

    update : document-features

---
+++ src/app/(main)/materials/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { DocumentUpload } from "@/components/learning/DocumentUpload";
+
+export default function MaterialsPage() {
+  return (
+    <div className="container mx-auto py-6">
+      <DocumentUpload />
+    </div>
+  );
+}
